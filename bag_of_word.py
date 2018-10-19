e804382f6078f3a8727d9bd17a5abb5a2825cd1b
Tann-chen <tianen.chen@outlook.com>
2018-10-18 23:51:22 -0400

    agency2vec

--- bag_of_word.py
+++ bag_of_word.py
@@ -3,75 +3,147 @@ import csv
 import json
 import re
 import traceback
+import pickle
 import requests
 from bs4 import BeautifulSoup
 
 
-file_path = "valid5000.csv"
-source_file = open(file_path, 'r', encoding='gb18030')
-reader = csv.reader(source_file)
-saved_data = ''
-regexp_number = re.compile('^[0-9a-zA-Z]*$')
-regexp_punct = re.compile("^[\s+\!\/_,$%^*(+\"\')]+|[:：+——()?【】“”！，。？、~@#￥%……&*（）. 〉《 》〔〕；～ ％]$")
-# stop_words_lst = ["市民", "来电", "咨询", "反映"]
-NI_suffix_lst = ["局", "处", "队", "所", "会"]
+
+def load_resource():
+	file_path = "valid5000.csv"
+	source_file = open(file_path, 'r', encoding='gb18030')
+	reader = csv.reader(source_file)
+
+	regexp_number = re.compile('^[0-9a-zA-Z]*$')
+	regexp_punct = re.compile("^[\s+\!\/_,$%^*(+\"\')]+|[:：+——()?【】“”！，。？、~@#￥%……&*（）. 〉《 》〔〕；～ ％]$")
+
+	stop_words_lst = ["市民", "来电", "咨询", "反映", "职能", "规定", "局"]
+	NI_suffix_tuple = ("局", "队", "所", "会", "中心", "部门")
+	inverse_index = {}
+	label_map = {}
+	instance_id = -1
+
+	for row in reader:
+		payload = {}
+		instance_id = instance_id + 1
+		label = row[12] # agency label
+		payload['s'] = row[7]	#query content
+		payload['f'] = 'xml'
+		payload['t'] = 'ner'
+		response = requests.post("http://127.0.0.1:12345/ltp", data=payload, timeout=5)
+		soup = BeautifulSoup(response.text, 'html.parser')
+		word_tags = soup.findAll('word')
+
+		buffers = []
+
+		for w in word_tags:
+			token = w['cont']
+			pos = w['pos']
+			ner = w['ne']
+
+			# rm stop words
+			if token in stop_words_lst and ner is 'O':
+				continue
+
+			# merge continuous nouns
+			if ner.startswith('B-') or ner.startswith('I-'):
+				buffers.append(token)
+				continue
+
+			if ner.startswith('E-'):
+				buffers.append(token)
+				token = ''.join(buffers)
+				buffers.clear()
+
+			# note the NER
+			if ner is not 'O':
+				pos = ner[-2:]
+
+			# filter numbers & punct
+			if regexp_number.match(token.strip()):
+				print("[INFO] invalid token : alphnum")
+				continue
+			if regexp_punct.match(token.strip()) or pos == 'wp':
+				print("[INFO] invalid token : punctuation")
+				continue
+
+			# custom rules
+			if (pos == 'j' or pos == 'n') and len(token) >=3 and token.endswith(NI_suffix_tuple):
+				pos = 'Ni'
+				print("[INFO] pos of token should be Ni : " + token)
+
+			# build inverse index
+			if pos not in ('Ni', 'Ns', 'n', 'j'):
+				print("exclusive :" + token + ":" + pos)   # print, but throw out
+			elif token in inverse_index.keys():
+				instances_set = inverse_index[token]
+				instances_set.add(instance_id)
+				print("add :" + token)
+			else:
+				new_set = set()
+				new_set.add(instance_id)
+				inverse_index[token] = new_set
+				print("add :" + token)
+
+		print("-----------------------------")
+		label_map[instance_id] = label
+
+	print("================ end =============")
+	print(len(list(inverse_index.keys())))
 
 
-for row in reader:
-	payload = {}
-	payload['s'] = row[7]
-	payload['f'] = 'xml'
-	payload['t'] = 'ner'
-	response = requests.post("http://127.0.0.1:12345/ltp", data=payload, timeout=5)
-	soup = BeautifulSoup(response.text, 'html.parser')
-	word_tags = soup.findAll('word')
-	tokens = []
-	buffers = []
+	with open('bow_inverse_index.pickle', 'wb') as f:
+		pickle.dump(inverse_index, f, pickle.HIGHEST_PROTOCOL)
 
-	for w in word_tags:
-		token = w['cont']
-		pos = w['pos']
-		ne = w['ne']
-
-		# rm stop words
-		# if token in stop_words_lst and ne is 'O':
-		# 	continue
-
-		# merge continuous nouns
-		if ne.startswith('B-') or ne.startswith('I-'):
-			buffers.append(token)
-			continue
-
-		if ne.startswith('E-'):
-			buffers.append(token)
-			token = ''.join(buffers)
-			buffers.clear()
-
-		# note the NER
-		if ne is not 'O':
-			pos = ne[-2:]
-
-		# filter numbers & punct
-		if regexp_number.match(token.strip()):
-			print("[INFO] invalid token : alphnum")
-			continue
-		if regexp_punct.match(token.strip()) or pos == 'wp':
-			print("[INFO] invalid token : punctuation")
-			continue
-
-		# custom rules
-		if pos == 'j' and token[-1:] in NI_suffix_lst:
-			pos = 'Ni'
-			print("[INFO] pos of token should be Ni : " + token)
-		
-		tokens.append(token + '^' + pos)
-	
-	doc_id = row[0]
-	saved_data = saved_data + doc_id + '  ' + ''.join(tokens) + '\n'
-
-with open('pos.txt', 'w') as file:
-	file.write(saved_data)
+	with open('instance_label.pickle', 'wb') as f:
+		pickle.dump(label_map, f, pickle.HIGHEST_PROTOCOL)
 
 
 
-	
+def build_bag_of_word():
+	index_file = "bow_inverse_index.pickle"
+	label_file = "instance_label.pickle"
+
+	with open(index_file, 'rb') as iif:
+		inverse_index = pickle.load(iif)
+
+	with open(label_file, 'rb') as lbf:
+		label_map = pickle.load(lbf)
+
+
+	instance_ids = list(label_map.keys())
+	tokens = list(inverse_index.keys())
+
+	# build matrix
+	matrix = []
+
+	for iid in instance_ids:
+		vector = [0]* len(tokens)
+		for kid in range(0, len(tokens)):
+			if iid in inverse_index[tokens[kid]]:
+				vector[kid] = 1
+
+		matrix.append(vector)
+
+	# build labels
+	labels = ['']* len(instance_ids)
+
+	for iid in instance_ids:
+		labels[iid] = label_map[iid]
+
+	with open('matrix.pickle', 'wb') as f:
+		pickle.dump(matrix, f, pickle.HIGHEST_PROTOCOL)
+
+	with open('labels.pickle', 'wb') as f:
+		pickle.dump(labels, f, pickle.HIGHEST_PROTOCOL)
+
+	with open('matrix.txt', 'w') as f:
+		f.write(json.dumps(matrix))
+
+
+
+
+
+if __name__ == '__main__':
+	load_resource()
+	build_bag_of_word()
--- word2vec.py
+++ word2vec.py
@@ -1,4 +1,6 @@
 import synonyms
+import pickle
+import json
 # from sklearn.cluster import KMeans
 # from sklearn.decomposition import PCA
 
@@ -6,9 +8,77 @@ import synonyms
 # source_file = open(file_path, 'r', encoding='gb18030')
 
 # print(synonyms.seg("精神文明建设指导委员会办公室"))
-# print(synonyms.v("市政市容管委会"))
-#print(synonyms.nearby("统计局"))
-print(synonyms.v("交警"))
+# print(synonyms.nearby("统计局"))
+
+NI_ends_one = ('局', '社','委', '办')
+NI_ends_two = ('大队', '支队','中心')
+
+with open("agency.txt", 'r', encoding='gb18030') as f:
+	data = f.read()
+
+agency_2_vec = {}
+
+agency_lst = data.split('\n')
+for row in agency_lst:
+	agency_name = row.split(',')[0]
+	print("[NEW] " + agency_name + "is handling")
+	if len(agency_name.strip()) == 0:
+		continue
+
+	vector = []
+	if_exception = False
+
+	try:
+		vector = list(synonyms.v(agency_name))
+	except KeyError:
+		print("[INFO] origin agency name not in vocabulary :" + agency_name)
+		if_exception = True
+
+	if if_exception:
+		if agency_name.endswith(NI_ends_one):
+			print("[WARN] change agency_name from : " + agency_name + " to: " + agency_name[:-1])
+			agency_name_modified = agency_name[:-1]
+		if agency_name.endswith(NI_ends_two):
+			print("[WARN] change agency_name from : " + agency_name + " to: " + agency_name[:-2])
+			agency_name_modified = agency_name[:-2]
+
+		segment_lst = synonyms.seg(agency_name_modified)[0]
+		print("[INFO] seg agency name to :" + str(segment_lst))
+		vector = [0] * 100
+
+		for s in segment_lst:
+			temp_vector = []
+			try:
+				temp_vector = synonyms.v(s)
+			except KeyError:
+				print("[INFO] segment not in vocabulary :" + s)
+			if temp_vector is not None and len(temp_vector) > 0:
+				for i in range(0, 100):
+					vector[i] = vector[i] + temp_vector[i]
+
+		# get average
+		for i in range(0, 100):
+			vector[i] = vector[i] / len(segment_lst)
+
+
+	# add to agency_2_vec
+	if sum(vector) != 0:
+		agency_2_vec[agency_name] = vector
+	else:
+		print("-----------  DEBUG :" + agency_name + "-------------")
+		print(vector)
+
+
+with open('agency_2_vec.pickle', 'wb') as f:
+	pickle.dump(agency_2_vec, f, pickle.HIGHEST_PROTOCOL)
+
+
+print("================ Result ==================")
+for k,v in agency_2_vec.items():
+	print(k)
+	print(v)
+	print("---------------------------")
+
 
 
 
