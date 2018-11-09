11bb9714b54667cc7f1fda8490b8f1406fd9207a
Tann-chen <tianen.chen@outlook.com>
2018-11-09 15:04:53 -0500

    test set tfidf api

--- tfidf/test_tfidf.py
+++ tfidf/test_tfidf.py
@@ -7,7 +7,7 @@ import random
 index_file = "tfidf_inverse_index.pickle"
 instance_tokens_file = "tfidf_instance_tokens.pickle"
 
-global tfidf_inverse_index
+#global tfidf_inverse_index
 global tfidf_log_inversed_df
 global tfidf_dimen_tokens
 global tfidf_dimen_order
@@ -38,23 +38,39 @@ random.shuffle(tfidf_dimen_order)
 
 del num_instances
 del instance_tokens
+del tfidf_inverse_index
 
 print("[INFO] tfidf model havev loaded...")
 
 
+# load instance tokens cache for test set
+global test_instance_tokens
+
+test_instance_tokens_file = "test_set_instance_tokens.pickle"
+with open(test_instance_tokens_file, 'rb') as itf:
+	test_instance_tokens = pickle.load(itf)
+
+
 def get_instance_tfidf_vector(instance_id, if_shuffled):
+	if instance_id not in test_instance_tokens.keys():
+		print("[ERROR] instance_id is invalid")
+
+	# get tokens in instace
+	tokens = test_instance_tokens[instance_id]
+
 	# tf-idf = log（1 + F(t,d)) * log(N / df) : N is number of total instances
 	vector = [0] * len(tfidf_dimen_tokens)
 
 	for i in range(0, len(tfidf_dimen_tokens)):
 		idf = tfidf_log_inversed_df[i]  # get log (N / df) for cache
+		
 		# get F(t,d)
 		tf = 0
-		inverse_index_value = tfidf_inverse_index[tfidf_dimen_tokens[i]]
-		for ele in inverse_index_value:
-			if ele[0] == instance_id:
-				tf = ele[1]
-				break
+		token = tfidf_dimen_tokens[i]
+		for t in tokens:
+			if t == token :
+				tf += 1
+
 		# calculate tf-idf
 		vector[i] = math.log( 1 + tf) * idf
 
@@ -72,10 +88,9 @@ def get_instance_tfidf_vector(instance_id, if_shuffled):
 
 
 if __name__ == '__main__':
-	
 
 	# test
-	result  = get_instance_tfidf_vector("197", True)
+	result  = get_instance_tfidf_vector("73", True)
 	print(result)
 		
 	
